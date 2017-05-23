ffd355ead765e404f4a4ebf82b96c9753ecba7fc
Predrag <pele-94@hotmail.com>
2017-05-23 13:19:50 +0200

    Set up data model

---
+++ TheAgencyEJB/ejbModule/model/Agent.java
@@ -0,0 +1,21 @@
+package model;
+
+import java.io.Serializable;
+
+@SuppressWarnings("serial")
+public abstract class Agent implements Serializable{
+	
+	private AID id;
+	
+
+	public abstract void handleMessage(ACLMessage message);
+	
+	public AID getId() {
+		return id;
+	}
+
+	public void setId(AID id) {
+		this.id = id;
+	}
+
+}
