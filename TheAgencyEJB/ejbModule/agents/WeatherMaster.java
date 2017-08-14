ec5f788763bfaf0c96bea04563a217824786e527
Predrag <djaps94@gmail.com>
2017-08-15 00:01:12 +0200

    Add weather.html

--- TheAgencyEJB/ejbModule/agents/WeatherMaster.java
+++ TheAgencyEJB/ejbModule/agents/WeatherMaster.java
@@ -2,13 +2,22 @@ package agents;
 
 import javax.ejb.Stateless;
 
+import model.ACLMessage;
+import model.Agent;
+
 
 @Stateless
-public class WeatherMaster {
+public class WeatherMaster extends Agent{
 
+	private static final long serialVersionUID = -7609277672062916060L;
 
-    public WeatherMaster() {
+	public WeatherMaster() {
        
     }
+
+	@Override
+	public void handleMessage(ACLMessage message) {
+		
+	}
 
 }
--- TheAgencyEJB/ejbModule/agents/WeatherSlave.java
+++ TheAgencyEJB/ejbModule/agents/WeatherSlave.java
@@ -2,12 +2,22 @@ package agents;
 
 import javax.ejb.Stateless;
 
-@Stateless
-public class WeatherSlave {
+import model.ACLMessage;
+import model.Agent;
 
+@Stateless
+public class WeatherSlave extends Agent {
     
-    public WeatherSlave() {
+	private static final long serialVersionUID = -7183133844763416517L;
+
+	public WeatherSlave() {
         
     }
+
+	@Override
+	public void handleMessage(ACLMessage message) {
+		
+		
+	}
 
 }
