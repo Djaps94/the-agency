311d842c62882809649fc9ef6e5d327cc3043a4c
Predrag <djaps94@gmail.com>
2017-08-14 23:56:58 +0200

    Create weather agents

---
+++ TheAgencyEJB/ejbModule/agents/WeatherMaster.java
@@ -0,0 +1,14 @@
+package agents;
+
+import javax.ejb.Stateless;
+
+
+@Stateless
+public class WeatherMaster {
+
+
+    public WeatherMaster() {
+       
+    }
+
+}
---
+++ TheAgencyEJB/ejbModule/agents/WeatherSlave.java
@@ -0,0 +1,13 @@
+package agents;
+
+import javax.ejb.Stateless;
+
+@Stateless
+public class WeatherSlave {
+
+    
+    public WeatherSlave() {
+        
+    }
+
+}
